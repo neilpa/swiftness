2756446b01300a1ae63cc10d36808638ec5f6bf5
Neil Pankey <neil@wire.im>
2014-07-09 01:05:51 -0700

    Define addressing modes

---
+++ src/AddressingMode.swift
@@ -0,0 +1,25 @@
+//
+//  AddressingMode.swift
+//  Swiftness
+//
+//  Created by Neil Pankey on 7/9/14.
+//  Copyright (c) 2014 Neil Pankey. All rights reserved.
+//
+
+import Foundation
+
+enum AddressingMode {
+    case Implicit
+    case Accumulator
+    case Immediate
+    case ZeroPage
+    case ZeroPageX
+    case ZeroPageY
+    case Relative
+    case Absolute
+    case AbsoluteX
+    case AbsoluteY
+    case Indirect
+    case IndexedIndirectX
+    case IndirectIndexedY
+}
