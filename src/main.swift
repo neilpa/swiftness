799d1829b13697fa8a9620b2c0acd93d8f8ed6fb
Neil Pankey <neil@wire.im>
2014-07-08 22:49:58 -0700

    Setup xcode project

---
+++ src/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swiftness
+//
+//  Created by Neil Pankey on 7/8/14.
+//  Copyright (c) 2014 Neil Pankey. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
