b6f0c41e24eddecb9f1ef0021d16be55ff1a13e8
Neil Pankey <neil@wire.im>
2014-07-11 00:05:16 -0700

    Math tests

--- src/main.swift
+++ src/main.swift
@@ -14,14 +14,40 @@ import Foundation
 // $0602    8d 00 02  STA $0200
 // $0605    00        BRK
 
-let mem = RAM(prog: [ 0xa9, 0xff, 0x8d, 0x00, 0x02, 0x00 ])
+var mem = RAM(prog: [ 0xa9, 0xff, 0x8d, 0x00, 0x02, 0x00 ])
 println(mem.data[0..<10])
 
-let cpu = CPU(memory: mem)
+var cpu = CPU(memory: mem)
 cpu.run()
 
 println(mem.data[0..<10])
-
 println(mem.data[0x200])
-
 println(cpu.pc)
+
+
+// Address  Hexdump   Dissassembly
+// -------------------------------
+// $0600    69 ff     ADC #$ff
+// $0602    69 01     ADC #$01
+
+mem = RAM(prog: [ 0x69, 0xff, 0x69, 0x01])
+cpu = CPU(memory: mem)
+cpu.run()
+
+println(cpu.a)
+println(cpu.getFlag(carryMask))
+println(cpu.getFlag(overflowMask))
+
+// Address  Hexdump   Dissassembly
+// -------------------------------
+// $0600    69 7f     ADC #$7f
+// $0602    69 05     ADC #$05
+
+mem = RAM(prog: [ 0x69, 0x7f, 0x69, 0x05])
+cpu = CPU(memory: mem)
+cpu.run()
+
+println(cpu.a)
+println(cpu.getFlag(carryMask))
+println(cpu.getFlag(overflowMask))
+
