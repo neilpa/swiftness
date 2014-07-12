c338bd03a7871f56d430a4dfb0a9425297a84f5c
Neil Pankey <neil@wire.im>
2014-07-11 23:12:57 -0700

    Ignore overflow operator

--- src/Register.swift
+++ src/Register.swift
@@ -15,3 +15,10 @@ extension Byte {
         return String(format: "%02X", self)
     }
 }
+
+// Add with allowed overflow that is ignored
+operator infix +! {}
+@infix func +! (lhs: Byte, rhs: Byte) -> Byte {
+    let (sum, _) = Byte.addWithOverflow(lhs, rhs)
+    return sum
+}
