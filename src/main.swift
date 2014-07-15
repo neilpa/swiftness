21a96bc368b6b5c9526ae7d1f19de4a4166401ae
Neil Pankey <neil@wire.im>
2014-07-15 01:16:09 -0700

    Pattern tile dump

--- src/main.swift
+++ src/main.swift
@@ -8,16 +8,48 @@
 
 import Foundation
 
-//let path = "/Users/neilpa/code/emu/nes/test/nestest.nes"
-let path = "/Users/neilpa/code/emu/roms/Roms/VS/VS Super Mario Bros (VS).nes"
+let path = "/Users/neilpa/code/emu/nes/test/nestest.nes"
+//let path = "/Users/neilpa/code/emu/roms/Roms/VS/VS Super Mario Bros (VS).nes"
 let cart = Cartridge(path: path)
 
-var cpu = CPU(memory: MemMap(cart: cart, ppu: PPU()))
-cpu.trace = true
+// Dump the pattern tables
 
-var count = 0
-while (true) {
-    // TODO Should return num cycles
-    print(String(format:"%05i", count++) + "  ")
-    cpu.step()
+let tileBytes = 16
+
+class PatternTile {
+    let memory: Memory
+    let offset: Address
+    
+    init(memory: Memory, offset: Address) {
+        self.memory = memory
+        self.offset = offset
+    }
+    
+    subscript(x: Int, y: Int) -> Byte {
+        let bit0 = (memory[offset + Address(x)] >> Byte(y)) & 1
+        let bit1 = (memory[offset + Address(x) + 8] >> Byte(y)) & 1
+        return (bit1 << 1) | bit0
+    }
 }
+
+for tile in 0..<256 {
+    let t = PatternTile(memory: cart.chrROM, offset: Address(tile * tileBytes))
+    for row in 0...7 {
+        for col in 0...7 {
+            print(t[row, col])
+        }
+        println()
+    }
+    
+    println()
+}
+
+//var cpu = CPU(memory: MemMap(cart: cart, ppu: PPU()))
+//cpu.trace = true
+//
+//var count = 0
+//while (true) {
+//    // TODO Should return num cycles
+//    print(String(format:"%05i", count++) + "  ")
+//    cpu.step()
+//}
